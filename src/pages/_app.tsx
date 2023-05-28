8094d8f665fedbf42e75bd7500292b2ef1d846da
BrunoBorges12 <brunoamix084@gmail.com>
2023-05-28 02:52:04 -0300

    configurando cores,fonts,e taiwindcss

---
+++ src/pages/_app.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins, Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --poppins-font: ${poppins.style.fontFamily};
+
+            --roboto-font: ${roboto.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className=" leading-6  text-    top-4  font-poppins"> oi</div>;
+}

64cd6193d046945eede08c5235f95f39d587ddc2
BrunoBorges12 <brunoamix084@gmail.com>
2023-05-30 10:40:32 -0300

    finalizando desktop  authForm

--- src/components/AuthForm/index.tsx
+++ src/components/AuthForm/index.tsx
@@ -6,16 +6,38 @@ type propsAuth = {
 };
 export const AuthForm = ({ signUp }: propsAuth) => {
   return (
-    <form className="bg-dark-700 flex flex-col     justify-center items-center  p-16 gap-8">
-      <h2>{signUp ? "Crie sua conta" : "Faça login"}</h2>
-      {signUp && <Input type="text" label="Seu nome" name="name" />}
-      <Input type="email" label="Email" name="email" />
-      <Input type="password" label="Senha" name="password" />
+    <form className=" shadow-2xl  shadow-dark-800 rounded bg-dark-700 flex flex-col   font-poppins   justify-center items-center  p-16 gap-8">
+      <h2 className=" text-xl text-light-100">
+        {signUp ? "Crie sua conta" : "Faça login"}
+      </h2>
+      {signUp && (
+        <Input
+          type="text"
+          label="Seu nome"
+          name="name"
+          placeholder="Exemplo: Maria da Silva"
+        />
+      )}
+      <Input
+        type="email"
+        label="Email"
+        name="email"
+        placeholder="Exemplo: exemplo@exemplo.com.br"
+      />
+      <Input
+        type="password"
+        label="Senha"
+        name="password"
+        placeholder="No mínimo 6 caracteres"
+      />
       <Button
         color="tomato-dark"
         size="medium"
         title={signUp ? "Criar conta" : "entra"}
       />
+      <span className="  hover:underline  text-light-100 font-medium">
+        <a href="">{signUp ? "Já tenho uma conta" : "Criar um conta"}</a>
+      </span>
     </form>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { AuthForm } from "@/components/AuthForm";
 export default function Home() {
   return (
     <div className="  w-screen h-screen  bg-dark-400  flex   flex-col items-center justify-center">
-      <AuthForm signUp={false} />
+      <AuthForm signUp={true} />
     </div>
   );
 }

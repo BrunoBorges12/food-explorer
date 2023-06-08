73701e36708fda23af9b117ab0354cbf5b395a35
BrunoBorges12 <brunoamix084@gmail.com>
2023-06-08 14:29:31 -0300

    arrumando o deskot de banner  e finalizando mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,17 @@ export default function Home() {
       <NavBar admin={true} />;
       <Container>
         <div className=" my-44  text-light-100 relative  h-full w-full   max-w-full select-none">
-          <div className="  rounded-lg banner w-full h-[160px]  flex lg:h-[260px]">
-            <div className="  w-1/2 lg:w-5/12 ">
+          <div className="  rounded-lg banner w-full h-[120px]  flex lg:h-[260px]">
+            <div className="  w-2/3 md:w-1/2 lg:w-1/2 ">
               <img
                 src="/doces.png  "
-                className=" bannerimg  top-[-52px] left-[-20px] relative md:w-[332px] lg:left-0 lg:top-[-110px] lg:h-auto lg:w-[632px]
+                className=" bannerimg  top-[-16px] left-[-20px] absolute w-[149px] md:relative md:w-[332px] lg:left-0 lg:top-[-110px] lg:h-auto lg:w-[632px]
               "
               ></img>
             </div>
-            <div className="  flex flex-col justify-center font-poppins lg:m-0  lg:items-center ">
-              <h1 className=" text-lg lg:text-2xl ">Sabores inigualáveis</h1>
-              <h2 className=" text-base">
+            <div className="  flex flex-col justify-center font-poppins lg:m-0  lg:items-center lg:1/2 ">
+              <h1 className=" text-base lg:text-2xl ">Sabores inigualáveis</h1>
+              <h2 className=" text-xs lg:text-base">
                 Sinta o cuidado do preparo com ingredientes selecionados
               </h2>
             </div>
